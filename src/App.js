201d7ae28a82f2c65dd04fb48326357ac1e2fb08
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-09-17 22:26:40 +0530

    from, pipe, map & subscribe

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {from} from 'rxjs'
+import { map } from 'rxjs/operators'
+
+
+let numberObservable = from([1, 2, 3, 4, 5]);
+let squaredNumbers = numberObservable.pipe(map(value => value * 2))
+
+console.log(squaredNumbers)
+
+squaredNumbers.subscribe(result => console.log(result));
 
 function App() {
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
